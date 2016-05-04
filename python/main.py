d4e71c51defc79bdb54d46db6a67d0fdf7385c86
Mitan <dmitrykharkovsky@gmail.com>
2016-05-04 17:56:33 +0800

    minor

--- python/main.py
+++ python/main.py
@@ -6,17 +6,17 @@ from KNNBaggingClassifier import EvaluateKNNBagging
 from DataNormalization import DataNormalizer
 from utils import DataSetReader
 
-if __name__ == '__main__':
-    filename = '../features/real1Features.csv'
-    filename = '../features/syn5Features.csv'
+def testWithFileName(filename):
+    print filename
     X, y = DataSetReader(filename)
 
-    scoring = None
-    #scoring = 'f1'
-    iris = load_iris()
-    X, y = iris.data, iris.target
+    # scoring = None
+    scoring = 'f1'
 
-    print "Ada Boost score " + str(EvaluateAdaBoost(X,y, scoring))
+    # iris = load_iris()
+    # X, y = iris.data, iris.target
+
+    print "Ada Boost score " + str(EvaluateAdaBoost(X, y, scoring))
 
     # Other two classifiers depend on distance, hence need to normalize data
     scaler = DataNormalizer(X)
@@ -24,3 +24,11 @@ if __name__ == '__main__':
     print "Random forest with feature extraction score " + str(EvaluateRF(scaled_X, y, scoring))
 
     print "KNN Bagging score " + str(EvaluateKNNBagging(scaled_X, y, scoring))
+    print
+
+if __name__ == '__main__':
+    filename = '../features/real1Features.csv'
+    testWithFileName(filename)
+    for i in range(1,6):
+        filename = '../features/syn' + str(i) + 'Features.csv'
+        testWithFileName(filename)
